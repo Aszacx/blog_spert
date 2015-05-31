6fc445ebd28cfc12f6001977d96e25b585baf383
issac.centeno <issac.centeno@spert.com.mx>
2015-05-31 18:46:11 -0500

    Actualización de Logo

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 				 */
 				do_action( 'twentyfifteen_credits' );
 			?>
-			<a href="<?php echo esc_url( __( 'http://spert.com.mx', 'twentyfifteen' ) ); ?>"><?php printf( __( '© Todos los Derechos Reservados  %s', 'twentyfifteen' ), 'Servicios Profesionales en Recursos Humanos y Tecnologías S.A. de C.V.' ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://spert.com.mx', 'twentyfifteen' ) ); ?>"><?php printf( __( '© Todos los Derechos Reservados  %s', 'twentyfifteen' ), 'SPERT S.A. de C.V.' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
